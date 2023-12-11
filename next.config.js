4340f0eda45c5d5166ff1dbd49d3df380dcecaaf
Halyna Harasymovych <halyna.harasymovych@tallium.com>
2023-12-11 10:43:38 +0200

    Updated keys images

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@ const nextConfig = {
   output: process.env.NODE_ENV === "development" ? undefined : "export",
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/,
+      test: /\.svg$/, 
       use: ['@svgr/webpack'],
     });
 
     return config;
   }, 
+
+  
 }
 
 module.exports = nextConfig
